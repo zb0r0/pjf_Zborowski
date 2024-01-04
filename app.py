6487d3171ab07f4732db21012594049750177c95
zb0r0 <krystin52@gmail.com>
2024-01-04 18:12:04 +0100

    ogolna poprawa kodu

--- app.py
+++ app.py
@@ -1,15 +1,38 @@
 from flask import Flask, render_template
 import calendar
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
-class Dzien:
-    def __init__(self, data, eventy):
-        self.data = data
-        self.eventy = eventy
+def poczatek_tygodnia(poczatek_semestru):
+    poczatek_semestru = datetime.strptime(poczatek_semestru, '%d.%m.%Y')
+    ilosc_dni_do_poczatku_tygodnia = (poczatek_semestru.weekday()) % 7
+    data_poczatku_tygodnia = poczatek_semestru - timedelta(days=ilosc_dni_do_poczatku_tygodnia)
 
-    def __str__(self):
-        return f"{self.data}: {', '.join(self.eventy)}"
+    return data_poczatku_tygodnia.strftime('%d.%m.%Y')
+
+
+def koniec_tygodnia(koniec_semestru):
+    koniec_semestru = datetime.strptime(koniec_semestru, '%d.%m.%Y')
+    ilosc_dni_do_konca_tygodnia = (6 - koniec_semestru.weekday()) % 7
+    data_konca_tygodnia = koniec_semestru + timedelta(days=ilosc_dni_do_konca_tygodnia)
+
+    return data_konca_tygodnia.strftime('%d.%m.%Y')
+
+def generowanie_dat(poczatek_kalendarza, koniec_kalendarza):
+    poczatek_kalendarza = datetime.strptime(poczatek_kalendarza, '%d.%m.%Y')
+    koniec_kalendarza = datetime.strptime(koniec_kalendarza, '%d.%m.%Y')
+
+    lista_dat = []
+    iteracja_dni = poczatek_kalendarza
+    while iteracja_dni <= koniec_kalendarza:
+        lista_dat.append([
+            calendar.day_name[iteracja_dni.weekday()],
+            iteracja_dni.strftime('%d.%m.%Y')  # Formatowanie dat do 'dd.mm.yyyy'
+        ])
+        iteracja_dni += timedelta(days=1)
+    return lista_dat
+
 
 def odczyt_terminarza(file_path):
     terminarz = []
@@ -18,46 +41,19 @@ def odczyt_terminarza(file_path):
             terminarz.append(line.strip().split(','))
     return terminarz
 
+
 @app.route('/')
 def widok_kalendarza():
-    poczatek_semestru = (2023, 10, 1)
-    koniec_semestru = (2024, 2, 29)
+    #reczne wpisywanie daty poczatku i konca semestru
+    poczatek_semestru = '01.10.2023'
+    koniec_semestru = '25.02.2024'
 
-    kalendarz = calendar.monthcalendar(poczatek_semestru[0], poczatek_semestru[1])
-
-    pierwszy_tydzien = next((week for week in kalendarz if any(day != 0 for day in week)), None)
-    if pierwszy_tydzien and pierwszy_tydzien[0] != 0:
-        pierwszy_tydzien = [0] * (7 - len(pierwszy_tydzien)) + pierwszy_tydzien
-
-    data_kalendarza = []
-    aktualna_data = poczatek_semestru
-
-    while aktualna_data <= koniec_semestru:
-        tydzien = []
-
-        for dzien in pierwszy_tydzien:
-            if dzien == 0:
-                tydzien.append('')
-            else:
-                data_str = f"{aktualna_data[0]}-{aktualna_data[1]:02d}-{dzien:02d}"
-                eventy = dodawanie_eventow_do_dnia(data_str, odczyt_terminarza('zajecia.txt'))
-                tydzien.append(eventy)
-
-        data_kalendarza.append(tydzien)
-
-        #Przesunięcie do pierwszego dnia następnego miesiąca
-        if aktualna_data[1] == 12:
-            aktualna_data = (aktualna_data[0] + 1, 1, 1)
-        else:
-            aktualna_data = (aktualna_data[0], aktualna_data[1] + 1, 1)
-
-    return render_template('kalendarz.html', data_kalendarza=data_kalendarza)
-
-def dodawanie_eventow_do_dnia(data_str, terminarz):
-    for dzien in terminarz:
-        if dzien[2] == data_str:  # Zmiana na indeks kolumny z datą
-            return ', '.join(dzien)
-    return ''
+    #funkcje obliczające pierwszy oraz ostatni dzien kalendarza, aby móc przedstawić go w przejrzysty sposób
+    poczatek_kalendarza = poczatek_tygodnia(poczatek_semestru)
+    koniec_kalendarza = koniec_tygodnia(koniec_semestru)
+    kalendarz = generowanie_dat(poczatek_kalendarza, koniec_kalendarza)
+    print(kalendarz)
+    return render_template('kalendarz.html', kalendarz=kalendarz)
 
 if __name__ == '__main__':
     app.run(debug=True)
